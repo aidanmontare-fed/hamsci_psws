6e438dc969155b2ba93c00217809cccecae15180
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2022-07-20 22:16:26 -0400

    creating

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='grape1',
+      version='0.1',
+      description='HamSCI Grape v1 Personal Space Weather Station Plotting Tools',
+      author='Nathaniel A. Frissell and Kristina V. Collins',
+      author_email='nathaniel.frissell@scranton.edu',
+      url='https://hamsci.org',
+      packages=['grape1'],
+      install_requires=requirements
+     )
